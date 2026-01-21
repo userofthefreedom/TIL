3ffdde0afa7bbaa450532c1be47ee39e91d3b805
Sejin_oh <sptrain6@gmail.com>
2026-01-22 00:00:34 +0900

    26.01.22 mincode update

--- min_code/7_2.py
+++ min_code/7_2.py
@@ -1,24 +1,23 @@
-# t = input()
-# for i in range(6):
-#     if i <= 2:
-#         for j in range(5):
-#             print(t,end="")
-#     else :
-#         for j in range(3):
-#             print(t,end="")
-#     print("")
+t = input()
+for i in range(6):
+    if i <= 2:
+        for j in range(5):
+            print(t,end="")
+    else :
+        for j in range(3):
+            print(t,end="")
+    print("")
 
 
 
 
-# t = [0]*6
-# lst_idx = list(map(int, input().split()))
-# for i in lst_idx:
-#     t[i]=1
-# print(*t)
+t = [0]*6
+lst_idx = list(map(int, input().split()))
+for i in lst_idx:
+    t[i]=1
+print(*t)
 
 
-# 97 <= x <= 122: 소문자 65 <= ord(txt_list[0]) <= 90
 
 txt_list = list(map(str,input().split()))
 if 65 <= ord(txt_list[0]) <= 90 and 65 <= ord(txt_list[1]) <= 90 :
@@ -27,3 +26,57 @@ elif 65 <= ord(txt_list[0]) <= 90 or 65 <= ord(txt_list[1]) <= 90 :
     print("대소문자")
 else:
     print("".join(f'{chr(i)}' for i in range(97,123)))
+
+
+
+
+txt_base = input()
+adding_char = ord(txt_base)
+char_list = []
+for i in range(3):
+    ready_list = []
+    for j in range(5):
+        ready_list.append(chr(adding_char))
+        adding_char += 1
+    char_list.append(ready_list)
+# char_list[2][2] = chr(ord(char_list[2][2])+32)
+char_list[2][2] = char_list[2][2].lower()
+print(char_list[2][2])
+
+
+
+char_list = list(map(str,input().split()))
+count_num = 0
+for i in char_list:
+    if 'A' <= i <= 'Z' :
+        count_num += 1
+
+if count_num == 3:
+    print("풍족하다")
+elif count_num == 0:
+    print("부족하다")
+else:
+    print("적절하다")
+
+
+lst_0 = [
+    [0,0,0,0],
+    [0,0,0,0]
+]
+x, y =map(int,input().split())
+lst_0[x][y] = 1
+print(*lst_0[0])
+print(*lst_0[1])
+
+
+
+
+
+lst_ready = list(map(int,input().split()))
+answer_list = []
+for i in range(3):
+    answer_list.append([lst_ready[i*2]+2,lst_ready[i*2+1]+2])
+for k in answer_list:
+    for j in k:
+        print(j,end=" ")
+    print("")
---
+++ min_code/8_1.py
@@ -0,0 +1,83 @@
+# x = int(input())
+# t = 1
+# while t <= x:
+#     print(t,end=" ")
+#     t += 1
+
+
+# st_list = list(map(int,input().split()))
+# for i in st_list:
+#     if i == 7:
+#         break
+#     print(i,end=" ")
+
+
+
+
+# def inp():
+#     global a, b
+#     a,b = map(int,input().split())
+
+# def out():
+#     t = 5
+#     while t <= a+b:
+#         print(t,end=" ")
+#         t += 1
+
+# inp()
+# out()
+
+
+
+
+
+
+# st_list = list(map(int,input().split()))
+# for i in range(len(st_list)-1,-1,-1):
+#     if st_list[i] == 7:
+#         print(st_list[i])
+#         break
+#     print(st_list[i],end=" ")
+
+
+
+
+# st_pr = [3,4,1,6,7,5]
+# cout_num = 0
+# while cout_num < len(st_pr):
+#     print(st_pr[cout_num],end=" ")
+#     cout_num += 1
+
+
+def inp():
+    global first_list
+    first_list = []
+    start_num = int(input())
+    for i in range(3):
+        addme = []
+        for j in range(4):
+            addme.append(start_num + j + (i*4))
+        first_list.append(addme)
+# def inp():
+#     global first_list
+#     start_num = int(input())
+#     nums = list(range(start_num, start_num + 12))
+#     first_list = [nums[i*4:(i+1)*4] for i in range(3)]
+# def make_grid():
+#     start_num = int(input())
+#     return [[start_num + i*4 + j for j in range(4)] for i in range(3)]
+
+# first_list = make_grid()
+
+def process():
+    for i in range(3):
+        for j in range(4):
+            first_list[i][j] += 1
+
+def out():
+    for i in first_list:
+        print(*i)
+
+inp()
+process()
+out()
